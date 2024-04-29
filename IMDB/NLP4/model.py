ad427e66606889ae620c8e5f413dfdd14fd00a42
Hymavathi G <Hgummudala@gmail.com>
2024-04-29 13:22:02 -0400

    Code

---
+++ IMDB/NLP4/model.py
@@ -0,0 +1,162 @@
+import math
+import torch
+import torch.nn.functional as F
+from torch import nn
+from transformers import AutoModel
+
+class Transformer(nn.Module):
+    def __init__(self, base_model, num_classes, input_size):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.linear = nn.Linear(base_model.config.hidden_size, num_classes)
+        self.dropout = nn.Dropout(0.5)
+        self.softmax = nn.Softmax(dim=1)
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        cls_feats = raw_outputs.last_hidden_state[:, 0, :]
+        predicts = self.softmax(self.linear(self.dropout(cls_feats)))
+        return predicts
+
+class Lstm_Model(nn.Module):
+    def __init__(self, base_model, num_classes, input_size):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.Lstm = nn.LSTM(input_size=self.input_size,
+                            hidden_size=320,
+                            num_layers=1,
+                            batch_first=True)
+        self.fc = nn.Sequential(nn.Dropout(0.5),
+                                nn.Linear(320, 80),
+                                nn.Linear(80, 20),
+                                nn.Linear(20, self.num_classes),
+                                nn.Softmax(dim=1))
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        tokens = raw_outputs.last_hidden_state
+        lstm_output, _ = self.Lstm(tokens)
+        outputs = lstm_output[:, -1, :]
+        outputs = self.fc(outputs)
+        return outputs
+
+class BiLstm_Model(nn.Module):
+    def __init__(self, base_model, num_classes, input_size):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.BiLstm = nn.LSTM(input_size=self.input_size,
+                              hidden_size=320,
+                              num_layers=1,
+                              batch_first=True,
+                              bidirectional=True)
+        self.fc = nn.Sequential(nn.Dropout(0.5),
+                                nn.Linear(320 * 2, 80),
+                                nn.Linear(80, 20),
+                                nn.Linear(20, self.num_classes),
+                                nn.Softmax(dim=1))
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        cls_feats = raw_outputs.last_hidden_state
+        outputs, _ = self.BiLstm(cls_feats)
+        outputs = outputs[:, -1, :]
+        outputs = self.fc(outputs)
+        return outputs
+
+class Rnn_Model(nn.Module):
+    def __init__(self, base_model, num_classes, input_size):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.Rnn = nn.RNN(input_size=self.input_size,
+                          hidden_size=320,
+                          num_layers=1,
+                          batch_first=True)
+        self.fc = nn.Sequential(nn.Dropout(0.5),
+                                nn.Linear(320, 80),
+                                nn.Linear(80, 20),
+                                nn.Linear(20, self.num_classes),
+                                nn.Softmax(dim=1))
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        cls_feats = raw_outputs.last_hidden_state
+        outputs, _ = self.Rnn(cls_feats)
+        outputs = outputs[:, -1, :]
+        outputs = self.fc(outputs)
+        return outputs
+
+class TextCNN_Model(nn.Module):
+    def __init__(self, base_model, num_classes):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+        # Define the hyperparameters
+        self.filter_sizes = [2, 3, 4]
+        self.num_filters = 2
+
+        # TextCNN
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(in_channels=1, out_channels=self.num_filters,
+                       kernel_size=(K, self.base_model.config.hidden_size)) for K in self.filter_sizes]
+        )
+        self.block = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(self.num_filters * len(self.filter_sizes), self.num_classes),
+            nn.Softmax(dim=1)
+        )
+
+    def conv_pool(self, tokens, conv):
+        tokens = conv(tokens)
+        tokens = F.relu(tokens)
+        tokens = tokens.squeeze(3)
+        tokens = F.max_pool1d(tokens, tokens.size(2))
+        out = tokens.squeeze(2)
+        return out
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        tokens = raw_outputs.last_hidden_state.unsqueeze(1)
+        out = torch.cat([self.conv_pool(tokens, conv) for conv in self.convs],
+                        1)
+        predicts = self.block(out)
+        return predicts
+
+class RCNN_Model(nn.Module):
+    def __init__(self, base_model, num_classes):
+        super().__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        for param in base_model.parameters():
+            param.requires_grad = True
+
+        # RCNN
+        self.conv1d = nn.Conv1d(in_channels=self.base_model.config.hidden_size, out_channels=256, kernel_size=3)
+        self.lstm = nn.LSTM(input_size=256, hidden_size=128, num_layers=1, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(256, num_classes)
+
+    def forward(self, inputs):
+        raw_outputs = self.base_model(**inputs)
+        hidden_states = raw_outputs.last_hidden_state.permute(0, 2, 1)
+        conv_out = self.conv1d(hidden_states).permute(0, 2, 1)
+        lstm_out, _ = self.lstm(conv_out)
+        logits = self.fc(lstm_out[:, -1, :])
+        return logits
